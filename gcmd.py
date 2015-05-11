34c213362a77e61e0842755d7055b035fc2045ef
Manuel Lorenzo <manuel@trovit.com>
2015-05-11 16:26:46 +0200

    Added no-newline option for one-liner replies

--- gcmd.py
+++ gcmd.py
@@ -19,6 +19,10 @@ def main():
     optParser.add_argument("-k", "--key", help="pattern in servername",
                            metavar="STRING", required=False, type=str,
                            dest="key", default='')
+    optParser.add_argument("-n", "--no-newline", help="don't add a new line"
+                           "with the servername in the output",
+                           required=False, action='store_false',
+                           dest="newline", default=True)
     optParser.add_argument("command", help="command line to execute",
                            metavar="shell command", nargs='*')
     args = optParser.parse_args()
@@ -30,7 +34,10 @@ def main():
         with racktables() as rt:
             rt.connect()
             for server in rt.getServersByName(args.key):
-                print('\n\033[1;33m=====> %s <=====\033[0m' % server[1])
+                if args.newline:
+                    print('\n\033[1;33m=====> %s <=====\033[0m' % server[1])
+                else:
+                    print('\033[1;33m%s\033[0m: ' % server[1], end='')
                 try:
                     sshOps.sshCmd(server[1], ' '.join(args.command))
                 except sshLoginException as sLE:
