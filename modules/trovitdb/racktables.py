9036eb6a9cc2b0265f8b556f593eb79f3b52449d
Manuel Lorenzo <manuel@trovit.com>
2015-05-20 09:57:13 +0200

    Added new functions for managing IPv4 space

--- modules/trovitdb/racktables.py
+++ modules/trovitdb/racktables.py
@@ -4,6 +4,8 @@
 """This module is used to manage objects stored in Racktables DB"""
 
 import re
+from struct import pack, unpack
+from socket import inet_ntoa, inet_aton
 from . import trovitdb
 
 _OS = {
@@ -18,6 +20,7 @@ _OS = {
 
 class racktables(trovitdb):
     _confSection = 'racktables'
+    _ipTypeSet = ['regular', 'shared', 'virtual']
 
     def getServersByName(self, srvName):
         """
@@ -143,3 +146,139 @@ class racktables(trovitdb):
             print("%s Wrong OS version \'%s\' for ID %s" %
                   (self._errorMsg, osversion, serverId))
         return
+
+    def getIfaceIPs(self, serverId, ifaceName):
+        """
+        Get IP addresses allocated to given server/iface
+        Keywords:
+            @serverId: Server Object ID
+            @ifaceName: iface name (example: eth0)
+        Return: list[str(ipaddr1), str(ipaddr2), ...]
+        """
+        ips = []
+        data = self.query("select ip from IPv4Allocation \
+                           where object_id = %s \
+                               and name = '%s';" %
+                          (serverId, ifaceName))
+        map(lambda x: ips.append(int2ip(x[0])), data)
+        return ips
+
+    def getServerIPs(self, serverId):
+        """
+        Get all IP addresses and interfaces for given server
+        Keywords:
+            @serverId: Server Object ID
+        Return: dict{str(if-name): list[str(ipaddr1), ...]}
+        """
+        interfaces = dict()
+        data = self.query("select name, ip from IPv4Allocation \
+                           where object_id = %s;" % serverId)
+        for iface, ip in data:
+            if iface in interfaces:
+                interfaces[iface].append(int2ip(ip))
+            else:
+                interfaces[iface] = [int2ip(ip)]
+        return interfaces
+
+    def getServerInterfaces(self, serverId):
+        """
+        Get the network interfaces and its MAC address for a server
+        Keywords:
+            @serverId: Server Object ID
+        Return: dict{str(iface-name): str(MAC-addr)}
+        """
+        interfaces = dict()
+        data = self.query("select name, l2address from Port \
+                           where object_id = %s;" % serverId)
+        for iface, mac in data:
+            interfaces[iface] = mac
+        return interfaces
+
+    def getIpInfo(self, ip):
+        """
+        Retrieve IP related info (serverId, server, port) from racktables
+        database.
+        Keywords:
+            @ip: (str) IP in human readable format
+        Return: list[tuple(int(serverId), str(serverName), str(port)), ...]
+        """
+        data = self.query("select Object.id, Object.name, IPv4Allocation.name \
+                           from Object \
+                           left join IPv4Allocation \
+                           on Object.id = IPv4Allocation.object_id \
+                           where IPv4Allocation.ip = %s;" % ip2int(ip))
+        return data
+
+    def isAllocatedIp(self, ip, ipType='all'):
+        """
+        Check if the given IP address is into IPv4Allocation table
+        Keywords:
+            @ip: (str) IP address in human readable format
+            @ipType: (str) racktables IP type [regular, shared, virtual, all]
+        Return: boolean
+        """
+        queryType = ','.join(self._ipTypeSet)
+        if ipType in self._ipTypeSet:
+            queryType = ipType
+        else:
+            raise self.trovitdbException("Wrong IP type %s. Choose: [all, %s]"
+                                         % (ipType, queryType))
+        data = self.query("select * from IPv4Allocation \
+                           where ip in %s;" % queryType)
+        if len(data) > 0:
+            return True
+        return False
+
+    def allocateIp(self, ip, serverId, port, ipType='regular'):
+        """
+        Allocate an IPv4 address
+        Keywords:
+            @ip: (str) IP address in human readable format
+            @serverId: (int) Server Object ID
+            @port: (str) Port name
+            @ipType: (str) One of (regular, shared, virtual)
+        Return:
+            null
+        """
+        if ipType not in self._ipTypeSet:
+            raise self.trovitdbException("Wrong IP type %s." % ipType)
+        self.query("insert into IPv4Allocation \
+                    (object_id, ip, name, type) \
+                    values (%s, %s, '%s', '%s');"
+                   % (serverId, ip2int(ip), port, ipType))
+        return
+
+    def updateIp(self, ip, serverId, port, ipType='regular'):
+        """
+        Update an IPv4 address data
+        Keywords:
+            @ip: (str) IP address in human readable format
+            @serverId: (int) Server Object ID
+            @port: (str) Port name
+            @ipType: (str) One of (regular, shared, virtual)
+        Return:
+            null
+        """
+        if ipType not in self._ipTypeSet:
+            raise self.trovitdbException("Wrong IP type %s." % ipType)
+        self.query("update IPv4Allocation \
+                    set object_id = %s, \
+                        name = '%s', \
+                        type = '%s' \
+                    where ip = %s;"
+                   % (serverId, port, ipType, ip2int(ip)))
+        return
+
+
+def ip2int(addr):
+    """
+    Transform an human readable IP address to integer
+    """
+    return unpack("!I", inet_aton(addr))[0]
+
+
+def int2ip(addr):
+    """
+    Transform an integer to a human readable IP address
+    """
+    return inet_ntoa(pack("!I", addr))
